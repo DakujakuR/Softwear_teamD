c2bc18c6699b44aee4123e851243dfb527849e07
DakujakuReiya <e185709@ie.u-ryukyu.ac.jp>
2019-11-14 11:39:56 +0900

    test dakujaku

---
+++ Calender_dakujaku/Calender_dakujaku/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Calender_dakujaku
+//
+//  Created by DakujakuReiya on 2019/11/14.
+//  Copyright © 2019 DakujakuReiya. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
